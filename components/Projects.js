3fbdb89044fa8c74fd6917984dadae44eb6541b1
Renuka Choudhary <renurunrun@gmail.com>
2022-07-20 16:16:01 +0530

    improved

--- components/Projects.js
+++ components/Projects.js
@@ -2,6 +2,7 @@ const Projects = () =>{
     return(
         <div>
            <h1 className ="H1">About Me</h1>
+           <hr/>
            <p className ="p1">hey there! My name is Renuka Choudhary. I'm from Kolkata, WestBengal. I've Completed my MCA from Kalyani Government Engineering College.</p>
         
         </div>
